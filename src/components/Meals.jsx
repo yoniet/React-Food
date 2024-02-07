d028f28fb8f25f2402d4870155c9d0fbfdd3101b
YoniEtedgi <yonie812@gmail.com>
2024-02-07 16:33:08 +0200

    Add MealItem component that is responsible for dysplaing a meal in list

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem.jsx";
 
 export default function Meals() {
     const [loadedMeals, setLoadedMeals] = useState([]);
 
-    async function fetchMeals() {
-        const response = await fetch('http://localhost:3000/meals');
-
-        if (!response.ok) {
-            // ...
+    useEffect(()=> {
+        async function fetchMeals() {
+            const response = await fetch('http://localhost:3000/meals');
+    
+            if (!response.ok) {
+                // ...
+            }
+    
+            const meals = await response.json();
+            setLoadedMeals(meals);
         }
+    
+        fetchMeals();
+    }, [])
 
-        const meals = await response.json();
-        setLoadedMeals(meals);
-    }
+
 
     return (
         <ul id="meals">
             {loadedMeals.map((meal) =>
-                <li key={meal.id}>{meal.name}</li>
+                <MealItem meal={meal} key={meal.id}/>
             )}
         </ul>
     );
